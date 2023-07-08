6faee8857699d26d2b80014584d76bab93cad2d9
aminDB81 <aminbaluchi67@gmail.com>
2023-07-08 19:18:17 +0330

    set variables and Format time

---
+++ index.js
@@ -0,0 +1,15 @@
+let startTime;
+let elapsedTime = 0;
+let timerInterval;
+// Format time to minute:second:milisecond
+function formatTime(milliseconds) {
+  const minutes = Math.floor(milliseconds / 60000);
+  const seconds = Math.floor((milliseconds % 60000) / 1000);
+  const millisecondsFormatted = Math.floor((milliseconds % 1000) / 10)
+    .toString()
+    .padStart(2, "0");
+
+  return `${minutes.toString().padStart(2, "0")}:${seconds
+    .toString()
+    .padStart(2, "0")}:${millisecondsFormatted}`;
+}
